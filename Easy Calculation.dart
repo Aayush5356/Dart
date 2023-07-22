bd31ff048c24b50f2663b8d52e67cff37bb498e2
Aayush Patel <133498952+Aayush014@users.noreply.github.com>
2023-07-22 16:29:27 +0530

    Create Easy Calculation.dart

---
+++ Easy Calculation.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main()
+{
+    int a = 20;
+    int b = 40;
+
+    print("Addition is ${a + b}");
+    print("Substraction is ${b - a}");
+    print("Multiplication is ${b * a}");
+    print("Division is ${b / a}");
+}
