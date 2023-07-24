b0ba222ced7d270487eb400cf681f9d2a6ea3354
Aayush Patel <133498952+Aayush014@users.noreply.github.com>
2023-07-24 18:39:57 +0530

    Create create a list contains all Indian states.dart

---
+++ create a list contains all Indian states.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+void main()
+{
+  print("Enter State Names");
+  List l1 = [];
+  for(int i=0; i < 26; i++)
+  {
+    String? x = stdin.readLineSync();
+    l1.add(x);
+    print(l1);
+  }
+}
