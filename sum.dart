8ee36bb70fd1ac9c278cfbccd601e15d1e75b7a4
Aayush Patel <133498952+Aayush014@users.noreply.github.com>
2023-08-06 14:12:23 +0530

    Update sum.dart

--- sum.dart
+++ sum.dart
@@ -1,71 +1,62 @@
 import 'dart:io';
 void main() {
-  // Define the 3x3 array.
-  int[][] array = [
+
+  var list = [
     [1, 2, 3],
     [4, 5, 6],
-    [7, 8, 9]
+    [7, 8, 9],
   ];
-  // Print the menu options.
-  print('Select an operation:');
+  print('Menu');
   print('1. Sum of all elements');
   print('2. Sum of specific row');
   print('3. Sum of specific column');
   print('4. Sum of diagonal elements');
-  print('5. Sum of antidiagonal elements');
-  // Get the user's choice.
-  int choice = int.parse(stdin.readLineSync());
-  // Switch on the user's choice.
+  print('5. Exit');
+
+  var choice = int.parse(stdin.readLineSync()!);
+
   switch (choice) {
     case 1:
-      // Sum of all elements.
-      int sum = 0;
-      for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-          sum += array[i][j];
+      var sum = 0;
+      for (var i = 0; i < list.length; i++) {
+        for (var j = 0; j < list[i].length; j++) {
+          sum += list[i][j];
         }
       }
-      print('The sum of all elements is $sum.');
+      print('The sum of all elements is $sum');
       break;
+
     case 2:
-      // Sum of specific row.
       print('Enter the row number:');
-      int row = int.parse(stdin.readLineSync());
-      int sum = 0;
-      for (int j = 0; j < 3; j++) {
-        sum += array[row][j];
+      var row = int.parse(stdin.readLineSync()!);
+      var sum = 0;
+      for (var i = 0; i < list[row].length; i++) {
+        sum += list[row][i];
       }
-      print('The sum of the elements in row $row is $sum.');
+      print('The sum of row $row is $sum');
       break;
+
     case 3:
-      // Sum of specific column.
       print('Enter the column number:');
-      int column = int.parse(stdin.readLineSync());
-      int sum = 0;
-      for (int i = 0; i < 3; i++) {; i < 3; i++) {
-        sum += array[i][column];
+      var column = int.parse(stdin.readLineSync()!);
+      var sum = 0;
+      for (var i = 0; i < list.length; i++) {
+        sum += list[i][column];
       }
-      print('The sum of the elements in column $column is $sum.');
+      print('The sum of column $column is $sum');
       break;
+
     case 4:
-      // Sum of diagonal elements.
-      int sum = 0;
-      for (int i = 0; i < 3; i++) {
-        sum += array[i][i];
+      var sum = 0;
+      for (var i = 0; i < list.length;i++) {
+        sum += list[i][i];
       }
-      print('The sum of the diagonal elements is $sum.');
+      print('The sum of diagonal elements is $sum');
       break;
+
     case 5:
-      // Sum of antidiagonal elements.
-      int sum = 0;
-      for (int i = 0; i < 3; i++) {
-        sum += array[i][2 - i];
-      }
-      print('The sum of the antidiagonal elements is $sum.');
       break;
     default:
-      print('Invalid choice.');
-  break;
+      print('Exit!');
   }
-}
 }
